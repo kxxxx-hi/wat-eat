bb8b9f013e0280a9de2fe10b61aff296fd42774a
kxxxx-hi <59868398+kxxxx-hi@users.noreply.github.com>
2025-10-10 10:32:12 +0800

    Update dishes.js

--- src/data/dishes.js
+++ src/data/dishes.js
@@ -338,5 +338,157 @@ export const dishes = [
   "cost": 14.9,
   "cuisine": "Indian",
   "description": "Tempered lentils, buttery finish."
+  },
+  {
+  "id": 35,
+  "name": "Basil Rice at Hawker",
+  "picture": "https://hot-thai-kitchen.com/wp-content/uploads/2022/10/pad-gaprao-beef-sq-2-500x375.jpg
+  ",
+  "marcoLikeability": 5,
+  "kexinLikeability": 4,
+  "cost": 7.2,
+  "cuisine": "Hawker",
+  "description": "Fragrant Thai basil stir-fry over rice."
+  },
+  {
+  "id": 36,
+  "name": "Lakeside Chicken Rice",
+  "picture": "🍗🍚",
+  "marcoLikeability": 5,
+  "kexinLikeability": 5,
+  "cost": 4.0,
+  "cuisine": "Hawker",
+  "description": "Classic poached chicken with aromatic rice."
+  },
+  {
+  "id": 37,
+  "name": "Large Fries at MacDomnald",
+  "picture": "https://www.mcdonalds.com.sg/sites/default/files/2025-02/French%20Fries%20-%20M.jpg
+  ",
+  "marcoLikeability": 4,
+  "kexinLikeability": 5,
+  "cost": 3.5,
+  "cuisine": "American",
+  "description": "Golden crisp fries, perfect for sharing."
+  },
+  {
+  "id": 38,
+  "name": "McSpicy at MacDomnald",
+  "picture": "https://www.mcdonalds.com.sg/sites/default/files/2023-02/1200x1200_MOP_BBPilot_McSpicy.png
+  ",
+  "marcoLikeability": 5,
+  "kexinLikeability": 4,
+  "cost": 6.5,
+  "cuisine": "American",
+  "description": "Crispy, fiery chicken burger classic."
+  },
+  {
+  "id": 39,
+  "name": "Chicken McSpicy 2pc at MacDomnald",
+  "picture": "https://www.mcdonalds.com.sg/sites/default/files/2024-06/1200x1200_MOP_CMC_Spicy_2pc_nobg_noblurb.png
+  ",
+  "marcoLikeability": 5,
+  "kexinLikeability": 4,
+  "cost": 8.5,
+  "cuisine": "American",
+  "description": "Two pieces of spicy fried chicken heat."
+  },
+  {
+  "id": 40,
+  "name": "Latte at Olla Specialty Coffee",
+  "picture": "https://static.wixstatic.com/media/e40fb4_400d13cdbdc4486ea69dae2b13ceac01~mv2.jpg/v1/crop/x_43,y_0,w_914,h_1000/fill/w_280,h_306,al_c,q_80,usm_0.66_1.00_0.01,enc_avif,quality_auto/Corporate%20mobile%20coffee%20paper%20cup%2001.jpg
+  ",
+  "marcoLikeability": 4,
+  "kexinLikeability": 5,
+  "cost": 5.0,
+  "cuisine": "Drinks",
+  "description": "Smooth espresso with silky steamed milk."
+  },
+  {
+  "id": 41,
+  "name": "Latte at Pocket",
+  "picture": "https://danielfooddiary.com/wp-content/uploads/2023/08/pocket12.jpg
+  ",
+  "marcoLikeability": 4,
+  "kexinLikeability": 5,
+  "cost": 5.0,
+  "cuisine": "Drinks",
+  "description": "Light roast latte with clean finish."
+  },
+  {
+  "id": 42,
+  "name": "Trio at Pocket",
+  "picture": "☕🍮🍰",
+  "marcoLikeability": 5,
+  "kexinLikeability": 5,
+  "cost": 8.0,
+  "cuisine": "Pocket",
+  "description": "Three-part dessert and coffee harmony."
+  },
+  {
+  "id": 43,
+  "name": "Hawaiian Pizza at Now Pizza",
+  "picture": "https://static.wixstatic.com/media/0abd0d_35cce31831bd42e79daf55edafa73cae~mv2.png/v1/fill/w_1891,h_1080,al_c,q_95,enc_auto/0abd0d_35cce31831bd42e79daf55edafa73cae~mv2.png
+  ",
+  "marcoLikeability": 4,
+  "kexinLikeability": 4,
+  "cost": 12.0,
+  "cuisine": "Italian",
+  "description": "Sweet pineapple, savory ham balance."
+  },
+  {
+  "id": 44,
+  "name": "Pepperoni Pizza at Now Pizza",
+  "picture": "https://static.wixstatic.com/media/0abd0d_6775bfc52d484407a2fdf177c5d2be1c~mv2.png/v1/fill/w_1891,h_1080,al_c,q_95,enc_auto/0abd0d_6775bfc52d484407a2fdf177c5d2be1c~mv2.png
+  ",
+  "marcoLikeability": 5,
+  "kexinLikeability": 4,
+  "cost": 12.0,
+  "cuisine": "Italian",
+  "description": "Crispy crust, spicy pepperoni comfort."
+  },
+  {
+  "id": 45,
+  "name": "Margherita Pizza at Now Pizza",
+  "picture": "https://static.wixstatic.com/media/0abd0d_ba71800a69aa4e20898f9196ec284b62~mv2.png/v1/fill/w_1891,h_1080,al_c,q_95,enc_auto/0abd0d_ba71800a69aa4e20898f9196ec284b62~mv2.png
+  ",
+  "marcoLikeability": 4,
+  "kexinLikeability": 5,
+  "cost": 12.0,
+  "cuisine": "Italian",
+  "description": "Tomato, mozzarella, and basil simplicity."
+  },
+  {
+  "id": 46,
+  "name": "Roujiamo (Pork) at Qinji",
+  "picture": "https://dec-pub-img.obs.ap-southeast-3.myhuaweicloud.com/shopimages/product/16725385057082512.jpeg
+  ",
+  "marcoLikeability": 5,
+  "kexinLikeability": 5,
+  "cost": 8.0,
+  "cuisine": "Chinese",
+  "description": "Chinese pork sandwich with rich aroma."
+  },
+  {
+  "id": 47,
+  "name": "Biangbiang (3 in 1) at Huangtudi",
+  "picture": "https://encrypted-tbn0.gstatic.com/images?q=tbn
+  :ANd9GcS7j0dLrBpRHM3rsqpDyK1sBwZToiXHs_8wIg&s",
+  "marcoLikeability": 5,
+  "kexinLikeability": 4,
+  "cost": 9.8,
+  "cuisine": "Chinese",
+  "description": "Wide noodles with chili, meat, and egg."
+  },
+  {
+  "id": 48,
+  "name": "Roujiamo at Huangtudi",
+  "picture": "https://encrypted-tbn0.gstatic.com/images?q=tbn
+  :ANd9GcTF9g-VAdDnQ9idyLMB8LIxaVZFT6TGwDY_3Q&s",
+  "marcoLikeability": 4,
+  "kexinLikeability": 4,
+  "cost": 5.0,
+  "cuisine": "Chinese",
+  "description": "Savory flatbread sandwich with tender meat."
   }
 ];
